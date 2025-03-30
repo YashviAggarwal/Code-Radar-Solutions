71b23c3c826c9f9ab2e6c798cfebafd5ac92cc33
Yashvi Aggarwal <aggarwalyashvi6@gmail.com>
2025-03-30 14:47:41 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
-int main(){
-    int n,arr[100],swapped;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        printf("%d",&arr[i]);
+
+int main() {
+    int n, arr[100], swapped, isSortedInitially = 1;
+
+    // Read the size of the array
+    scanf("%d", &n);
+
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for(int i=0;i<n-1;i++){
-        swapped=0;
-        for(int j=0;j<=n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-                swapped=1;
+
+    // Bubble Sort Algorithm
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0; // Reset swap flag at the start of each pass
+        for (int j = 0; j < n - i - 1; j++) { // Fixed condition
+            if (arr[j] > arr[j + 1]) { // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+                isSortedInitially = 0; // Mark that a swap was made
             }
         }
-        if(swapped==0){
-        break;
+        if (swapped == 0) break; // Stop early if already sorted
     }
-    }
-    
-    if (swapped == 0) {
+
+    // Print if the array was sorted or not
+    if (isSortedInitially) {
         printf("Not Sorted\n");
     } else {
         printf("Sorted\n");
     }
+
     return 0;
 }
